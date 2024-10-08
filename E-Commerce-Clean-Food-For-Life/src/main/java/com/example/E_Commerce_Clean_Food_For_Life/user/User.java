9e9443cdaa7026aef536546784c045009341179b
AlexMound <caophong3107aa@gmail.com>
2024-10-09 06:55:10 +0700

    Update reomve username unique and readjust Front-end Register notification

--- E-Commerce-Clean-Food-For-Life/src/main/java/com/example/E_Commerce_Clean_Food_For_Life/user/User.java
+++ E-Commerce-Clean-Food-For-Life/src/main/java/com/example/E_Commerce_Clean_Food_For_Life/user/User.java
@@ -16,7 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false, unique = true)
+    @Column(name = "username", nullable = false)
     @NotEmpty(message = "Tên người dùng là bắt buộc")
     private String username;
 
